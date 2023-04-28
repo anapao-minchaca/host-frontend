85d447406ed9a60c2a50d8d7fae587afbf493646
Luis Emilio Alcántara Guzmán <44034926+EmilioAG99@users.noreply.github.com>
2023-04-28 16:59:50 -0600

    prueba-host

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [info, setInfo] = useState("");
+  const getElements = async () => {
+    const response = await axios.get("http://localhost:4000/bookdata");
+    console.log(response.data);
+    setInfo(JSON.stringify(response.data));
+  };
+
+  useEffect(() => {
+    getElements();
+  }, []);
+  return <div className="App">{info ?? <>{info}</>}</div>;
 }
 
 export default App;
